e020f03a58882f7df490d67c4efadb7410924c11
Min-Kyu Lee <mimmyum@naver.com>
2018-03-23 14:29:14 +0900

    Firstapp first - git

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet var view1: UIView!
+    
+    var now : Int = 0
+    let MessageArray = ["May the force be with you", "Live long and prosper",101,"To infinity and beyond", "Space is big. you just won't believe how vastly, hugely, mindbogglinly big it is and the world is very big and huge wonderful word is worth protect", 10] as [Any]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label1.lineBreakMode = .byWordWrapping
+        label1.numberOfLines = 0
+        label1.text = "Hello, world!"
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func changeColor(){
+        now+=1
+        if let temp = MessageArray[now%(MessageArray.count)] as? String{
+            self.label1.text = MessageArray[now%(MessageArray.count)] as! String
+        }else{
+            now += 1
+        }
+        
+        
     }
-
-
+    
 }
 
